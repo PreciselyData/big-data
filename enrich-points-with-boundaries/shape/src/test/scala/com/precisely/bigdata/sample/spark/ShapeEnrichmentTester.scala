2d17dbc6c3a5fd88188e8f4b3092c6a6b5ada4a9
Dustin <dustin.clark@precisely.com>
2020-09-25 14:06:44 -0400

    Update to a scalatest version that is compatible with 2.11 and 2.12 (#2)
    * Update to a scalatest version that is compatible with 2.11 and 2.12
    
    * Switch scalatests back to junitrunner. Add more dependencies to testjar.

--- enrich-points-with-boundaries/shape/src/test/scala/com/precisely/bigdata/sample/spark/ShapeEnrichmentTester.scala
+++ enrich-points-with-boundaries/shape/src/test/scala/com/precisely/bigdata/sample/spark/ShapeEnrichmentTester.scala
@@ -16,11 +16,12 @@ package com.precisely.bigdata.sample.spark
 import java.util.logging.{ConsoleHandler, Level, Logger}
 
 import org.junit.runner.RunWith
-import org.scalatest.junit.JUnitRunner
-import org.scalatest.{BeforeAndAfterAll, FunSuite}
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.BeforeAndAfterAll
+import org.scalatestplus.junit.JUnitRunner
 
 @RunWith(classOf[JUnitRunner])
-class ShapeEnrichmentTester extends FunSuite with BeforeAndAfterAll {
+class ShapeEnrichmentTester extends AnyFunSuite with BeforeAndAfterAll {
   private val parquetLogger = Logger.getLogger("org.apache.parquet") //holding on to logger to persist log level changes
 
   override def beforeAll() {
