faef602daf2b8840479bcdb73c1a8669be6fde3b
Praveen Kumar <praveen.kumar1@precisely.com>
2021-06-11 18:01:12 +0530

    Adding another testing driver

--- multipass-addressing/src/test/scala/com/precisely/bigdata/sample/spark/VerificationDriver.scala
+++ multipass-addressing/src/test/scala/com/precisely/bigdata/sample/spark/VerificationDriver.scala
@@ -21,10 +21,27 @@ object VerificationDriver {
     val sparkConf = new SparkConf()
     sparkConf.setIfMissing("spark.master", "local[*]")
     sparkConf.set("spark.sql.autoBroadcastJoinThreshold", "-1")
+    var sparkMajorVersion = 2
+    try{
+
+      sparkMajorVersion=org.apache.spark.SPARK_VERSION.split('=')(0).split('.')(0).toInt
+
+    }catch {
+      case ex:Exception => {
+        println("Exception during Spark Version detection " + ex)
+      }
+      case ex: Throwable =>println("Found a unknown exception"+ ex)
+    }
     val session = SparkSession.builder()
       .config(sparkConf)
       .getOrCreate()
-
+    
+    if(sparkMajorVersion == 3){
+      session.conf.set("spark.sql.legacy.allowUntypedScalaUDF",true);
+      println("Detected Spark Major version as 3. Therefore setting the flag")
+    }else {
+      println("Continue with Spark Major version 2. Therefore flag is not set.")
+    }
     val resultDataPath = args(0)
 
     val resultDF = session.read.option("header", "true").csv(resultDataPath)
